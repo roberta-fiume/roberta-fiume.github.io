9e717617e03466ab426293f1be2c705b26e85f7c
roberta-fiume <robertaf898@gmail.com>
2018-05-20 19:12:25 +0100

    Github changes the css about the layout colors of My portfolio

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,19 +1,11 @@
 
-$(document).ready(function() {
 
- /* Navigation scroll */
-    $(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
-        }
-    });
-});
-});
+
+function hideAndShow() {
+  var idMenu = document.getElementById("menu");
+  if (idMenu.style.display === "none") {
+    idMenu.style.display = "block";
+  } else {
+    idMenu.style.display = "none";
+  }
+}
